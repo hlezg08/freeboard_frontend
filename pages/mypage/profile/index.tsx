c070307cbcf4adcdb86ec512baa3c47c0f1457ad
hlezg08 <hyelin132@gmail.com>
2022-07-28 21:25:39 +0900

    [C] 회원정보 변경

--- pages/mypage/profile/index.tsx
+++ pages/mypage/profile/index.tsx
@@ -1,4 +1,6 @@
+import { gql, useMutation } from "@apollo/client";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import ButtonBlack from "../../../src/components/commons/buttons/black";
 import { withAuth } from "../../../src/components/commons/hocs/withAuth";
 import { useMoveToPage } from "../../../src/components/commons/hooks/useMoveToPage";
@@ -11,18 +13,57 @@ const Wrapper = styled.div`
 `;
 const Contents = styled.div`
   width: 100%;
+  display: grid;
+  gap: 1.5rem;
+`;
+const UPDATE_USER = gql`
+  mutation updateUser($updateUserInput: UpdateUserInput!) {
+    updateUser(updateUserInput: $updateUserInput) {
+      email
+      name
+    }
+  }
 `;
 
 function MyPageProfilePage() {
   const { onClickMoveToPage } = useMoveToPage();
+  const [updateUser] = useMutation(UPDATE_USER);
+  const [name, setName] = useState("");
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+  const onClickSubmit = async () => {
+    try {
+      await updateUser({
+        variables: {
+          updateUserInput: {
+            name,
+          },
+        },
+      });
+      alert("변경되었습니다!");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <Wrapper>
       <MypageSidebar />
       <Contents>
-        <ButtonBlack
-          title="비밀번호 변경"
-          onClick={onClickMoveToPage(`/mypage/profile/password`)}
-        />
+        <div>
+          <ButtonBlack
+            title="비밀번호 변경"
+            onClick={onClickMoveToPage(`/mypage/profile/password`)}
+          />
+        </div>
+        <div>
+          <input
+            onChange={onChangeName}
+            placeholder="변경할 닉네임을 입력하세요."
+          />
+          <ButtonBlack onClick={onClickSubmit} title="정보 변경" />
+        </div>
       </Contents>
     </Wrapper>
   );
