d46c70ef9ca12e4cebbb6d2e68b7629ad787b89f
hlezg08 <hyelin132@gmail.com>
2022-09-17 00:44:17 +0900

    [D] firebase config, 샘플 styles/css 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,14 @@
 import { AppProps } from "next/app";
-import "antd/dist/antd.css";
 import Layout from "../src/components/commons/layout";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
+import "antd/dist/antd.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { initializeApp } from "firebase/app";
 import ApolloSetting from "../src/components/commons/apollo";
 import { RecoilRoot } from "recoil";
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC_BF7fkwFhvj81mFNitZL7ZWOs0PHHvh0",
-  authDomain: "lynn-frontend.firebaseapp.com",
-  projectId: "lynn-frontend",
-  storageBucket: "lynn-frontend.appspot.com",
-  messagingSenderId: "324259781515",
-  appId: "1:324259781515:web:992a6b6136ee078f906d8e",
-};
-
-export const firebaseApp = initializeApp(firebaseConfig);
-
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ApolloSetting>
@@ -34,4 +21,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default App;
