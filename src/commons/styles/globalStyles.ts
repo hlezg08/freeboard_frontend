4f2982e686e458044c704cccf9630c13c9c15186
hlezg08 <hyelin132@gmail.com>
2022-09-22 01:08:18 +0900

    [C] 반응형 디자인 breakPoints 추가

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -1,8 +1,16 @@
 import { css } from "@emotion/react";
+import { breakPoints } from "./media";
 
 export const globalStyles = css`
   * {
     margin: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+    @media ${breakPoints.mobile} {
+      font-size: 14px;
+    }
   }
 `;
